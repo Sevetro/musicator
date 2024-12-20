fc2d410ee7ff662d66dfc2e519bcb49d0763cb93
ukasz Gil <lukasz.gil@4soft.co>
2024-12-21 00:34:12 +0100

    feat: tailwind-merge

---
+++ src/app/projects/[id]/_components/sampler.tsx
@@ -0,0 +1,13 @@
+import { loaded, Player } from "tone";
+
+export default function Sampler() {
+  const player = new Player(
+    "https://tonejs.github.io/audio/berklee/gong_1.mp3",
+  ).toDestination();
+
+  loaded().then(() => {
+    // player.start();
+  });
+
+  return <div>dupa</div>;
+}
