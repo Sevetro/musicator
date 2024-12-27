dc40757d2d66d5e0654746c8490f46e0e056e14c
ukasz Gil <lukasz.gil@4soft.co>
2024-12-27 21:59:01 +0100

    feat: add credentials

--- src/app/register/_data/register-user.ts
+++ src/app/register/_data/register-user.ts
@@ -11,6 +11,7 @@ async function registerUser(body: RegisterUserData) {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
 
     body: JSON.stringify(body),
   });
