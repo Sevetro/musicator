844b510595c2e411c244e522e60d1f44e2ebbd4f
ukasz Gil <lukasz.gil@4soft.co>
2025-01-10 13:07:31 +0100

    refactor: rename

---
+++ src/api/api-urls.ts
@@ -0,0 +1,5 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export const registerUserApiUrl = `${API_URL}/register`;
+export const confirmEmailApiUrl = `${API_URL}/confirm_email`;
+export const loginApiUrl = `${API_URL}/login`;
