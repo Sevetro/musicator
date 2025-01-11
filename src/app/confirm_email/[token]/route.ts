729267c5881e0838b062c99acbcaf068f59e7f7f
ukasz Gil <lukasz.gil@4soft.co>
2025-01-11 18:19:04 +0100

    try 4

--- src/app/confirm_email/[token]/route.ts
+++ src/app/confirm_email/[token]/route.ts
@@ -1,9 +1,39 @@
+import { confirmEmailApiUrl } from "@/api/api-urls";
+import { cantReachApiErrorCode } from "@/shared/error-codes";
+import { throwApiError } from "@/utils/throw-api-error";
+import { redirect } from "next/navigation";
+
+interface Props2 {
+  params: Promise<{ token: string }>;
+}
+
 interface Props {
   params: {
     token: string;
   };
 }
 
-export async function GET(request: Request, { params }: Props) {
-  console.log(params);
+export async function GET(request: Request, { params: { token } }: Props) {
+  console.log(`inside validateEmailToken function`);
+
+  let res: Response;
+
+  try {
+    console.log(`confirmEmailApiUrl: `, confirmEmailApiUrl);
+    console.log(`token: `, token);
+
+    res = await fetch(`${confirmEmailApiUrl}/${token}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json", //added
+      },
+    });
+    console.log(`validateEmail response: `, res);
+  } catch (err) {
+    throw new Error(cantReachApiErrorCode, { cause: err });
+  }
+
+  if (!res.ok) await throwApiError(res);
+
+  redirect("/confirmation_success");
 }
