aebd4410d90f8d5e9e69c4a7289610cf8628af7f
ukasz Gil <lukasz.gil@4soft.co>
2025-01-16 18:13:23 +0100

    disable login

--- src/composite-components/login-form/data/login-user.ts
+++ src/composite-components/login-form/data/login-user.ts
@@ -2,26 +2,23 @@ import { useMutation } from "@tanstack/react-query";
 
 import { LoginUserData } from "@/shared/entities/user";
 import { loginApiUrl } from "@/api/api-urls";
-import { throwApiError } from "@/utils/throw-api-error";
+import { throwMusicatorApiError } from "@/utils/throw-api-error";
 import { cantReachApiErrorCode } from "@/shared/error-codes";
 
 async function loginUser(body: LoginUserData) {
-  let res: Response;
-
-  try {
-    res = await fetch(loginApiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-      // credentials: "include", //TODO: should be used?
-    });
-  } catch (err) {
-    throw new Error(cantReachApiErrorCode);
-  }
-
-  if (!res.ok) await throwApiError(res);
+  // let res: Response;
+  // try {
+  //   res = await fetch(loginApiUrl, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(body),
+  //   });
+  // } catch (err) {
+  //   throw new Error(cantReachApiErrorCode);
+  // }
+  // if (!res.ok) await throwMusicatorApiError(res);
 }
 
 export const useLoginUser = () =>
